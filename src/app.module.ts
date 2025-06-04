51e80cd584e9ee6f5b17115ba00daaebd96aad8c
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-04 03:38:34 -0300

    feat(app): add missing modules to AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { DynamodbModule } from './database/dynamodb.module';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
+import { EventModule } from './events/events.module';
+import { SubscriptionModule } from './subscriptions/subscriptions.module';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [ConfigModule.forRoot(), DynamodbModule, AuthModule, UsersModule],
   controllers: [],
   providers: [],
 })

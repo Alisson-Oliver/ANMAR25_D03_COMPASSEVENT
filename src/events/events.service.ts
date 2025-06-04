c2e0c70f35c50dee3fc04cb204f6432d5c4455d7
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-04 11:49:01 -0300

    feat(events): implement create method for event creation with email notification

---
+++ src/events/events.service.ts
@@ -0,0 +1,80 @@
+import {
+  BadRequestException,
+  ConflictException,
+  ForbiddenException,
+  Injectable,
+  Put,
+} from '@nestjs/common';
+import { DynamoDBService } from '../database/dynamodb.service';
+import { SESMailService } from '../emails/aws-ses.service';
+import { CreateEventDto } from './dto/create-event.dto';
+import { v4 as uuid } from 'uuid';
+import {
+  GetCommand,
+  PutCommand,
+  ScanCommand,
+  UpdateCommand,
+} from '@aws-sdk/lib-dynamodb';
+import { eventCreatedEmailTemplate } from '../emails/templates/events/event-created-email.template';
+import { PatchEventDto } from './dto/patch-events.dto';
+import { Status } from '../common/enum/status.enum';
+import { UserService } from '../users/users.service';
+import { eventDeletedEmailTemplate } from '../emails/templates/events/event-deleted-email.template';
+import { Role } from '../common/enum/roles.enum';
+
+@Injectable()
+export class EventService {
+  private readonly tableName = process.env.DYNAMODB_TABLE_EVENTS || 'events';
+
+  constructor(
+    private readonly dynamoDBService: DynamoDBService,
+    private readonly sesMailService: SESMailService,
+    private readonly userService: UserService,
+  ) {}
+
+  async create(data: CreateEventDto, organizer: any, imageUrl: string) {
+    try {
+      if (await this.nameExists(data.name)) {
+        throw new ConflictException('event name already exists');
+      }
+
+      if (!organizer.id) {
+        throw new BadRequestException('organizerId is required');
+      }
+
+      const event = {
+        id: uuid(),
+        organizerId: organizer.id,
+        ...data,
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+        imageUrl: imageUrl,
+        deletedAt: null,
+        status: Status.ACTIVE,
+      };
+
+      await this.dynamoDBService.client.send(
+        new PutCommand({
+          TableName: this.tableName,
+          Item: event,
+        }),
+      );
+
+      await this.sesMailService.sendEmail(
+        organizer.email,
+        `🎉  Event Created Successfully: ${event.name}`,
+        eventCreatedEmailTemplate(
+          organizer.name,
+          event.name,
+          new Date(event.date).toISOString(),
+          event.description,
+          imageUrl,
+        ),
+      );
+
+      return event;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+}

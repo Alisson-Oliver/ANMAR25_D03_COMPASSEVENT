f6a08e5aa6a2cd3cd3bd1bfcb221fc9ef27c32d1
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-04 11:44:11 -0300

    feat(subscriptions): implement create subscription logic with email confirmation and iCal attachment

---
+++ src/subscriptions/subscriptions.service.ts
@@ -0,0 +1,124 @@
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { DynamoDBService } from '../database/dynamodb.service';
+import { ICalendarAttachment, SESMailService } from '../emails/aws-ses.service';
+import { GetCommand, PutCommand, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { v4 as uuid } from 'uuid';
+import { Status } from '../common/enum/status.enum';
+import { UserService } from '../users/users.service';
+import { EventService } from '../events/events.service';
+import { CreateSubscriptionDto } from './dto/create-subscription.dto';
+import { subscriptionCreatedEmailTemplate } from '../emails/templates/subscriptions/subscription-created-email.template';
+import { generateICalFile } from '../common/utils/generate-ical-file.util';
+import { subscriptionDeletedEmailTemplate } from '../emails/templates/subscriptions/subscription-deleted-email.template';
+
+@Injectable()
+export class SubscriptionService {
+  private readonly tableName =
+    process.env.DYNAMODB_TABLE_SUBSCRIPTIONS || 'subscriptions';
+
+  constructor(
+    private readonly dynamoDBService: DynamoDBService,
+    private readonly sesMailService: SESMailService,
+    private readonly userService: UserService,
+    private readonly eventService: EventService,
+  ) {}
+
+  async create(data: CreateSubscriptionDto) {
+    const subscription = {
+      id: uuid(),
+      ...data,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+      deletedAt: null,
+      status: Status.ACTIVE,
+    };
+
+    if (await this.subscriptionExists(data.user_id, data.event_id)) {
+      throw new BadRequestException('subscription already exists');
+    }
+
+    const user = await this.userService.findById(data.user_id);
+
+    if (user.status === Status.INACTIVE) {
+      throw new ForbiddenException('user is not active');
+    }
+
+    if (user.emailVerified === false) {
+      throw new ForbiddenException('user email is not verified');
+    }
+
+    const event = await this.eventService.findById(data.event_id);
+
+    if (event.status === Status.INACTIVE) {
+      throw new ForbiddenException('event is not active');
+    }
+
+    if (event.date < new Date().toISOString()) {
+      throw new ForbiddenException('event date is in the past');
+    }
+
+    const organizer = await this.userService.findById(event.organizerId);
+
+    await this.dynamoDBService.client.send(
+      new PutCommand({
+        TableName: this.tableName,
+        Item: subscription,
+      }),
+    );
+
+    const eventDetailsForIcal = {
+      id: event.id,
+      name: event.name,
+      description: event.description,
+      startDate: new Date(event.date),
+      organizer: organizer.name,
+      organizerEmail: organizer.email,
+    };
+
+    const icalString = generateICalFile(eventDetailsForIcal);
+
+    const icalAttachment: ICalendarAttachment = {
+      fileName: 'invite.ics',
+      content: icalString,
+      method: 'REQUEST',
+    };
+
+    await this.sesMailService.sendEmail(
+      user.email,
+      `🎟️  Registration Confirmed - ${event.name}`,
+      subscriptionCreatedEmailTemplate(
+        user.name,
+        event.name,
+        event.date,
+        event.imageUrl,
+      ),
+      icalAttachment,
+    );
+
+    return subscription;
+  }
+
+  async subscriptionExists(userId: string, eventId: string) {
+    try {
+      const result = await this.dynamoDBService.client.send(
+        new ScanCommand({
+          TableName: this.tableName,
+          FilterExpression: 'user_id = :user_id AND event_id = :event_id',
+          ExpressionAttributeValues: {
+            ':user_id': userId,
+            ':event_id': eventId,
+          },
+        }),
+      );
+
+      return (result.Count ?? 0) > 0;
+    } catch (error) {
+      throw new BadRequestException(error.message);
+    }
+  }
+}

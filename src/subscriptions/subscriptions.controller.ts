cdb088c24b060a66d8cebba6049c18866a307159
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-04 11:45:14 -0300

    feat(subscriptions): add SubscriptionController with findAllById method

---
+++ src/subscriptions/subscriptions.controller.ts
@@ -0,0 +1,29 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
+import { SubscriptionService } from './subscriptions.service';
+import { CreateSubscriptionDto } from './dto/create-subscription.dto';
+import { RoleGuard } from '../common/guards/role.guard';
+import { AuthGuard } from '../common/guards/auth.guard';
+import { Roles } from '../common/decorators/roles.decorator';
+import { Role } from '../common/enum/roles.enum';
+
+@Controller('/subscriptions')
+export class SubscriptionController {
+  constructor(private readonly subscriptionService: SubscriptionService) {}
+
+  @Get()
+  @Roles(Role.ADMIN, Role.ORGANIZER, Role.PARTICIPANT)
+  @UseGuards(AuthGuard, RoleGuard)
+  async findAllById(@Req() req) {
+    return await this.subscriptionService.findAllById(req.user.id);
+  }
+}

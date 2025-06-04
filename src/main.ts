6f1fa530cc738e8248af8ec97cc05c39971c0366
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-04 12:41:27 -0300

    feat(main): integrate Swagger for API documentation

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,19 @@ async function bootstrap() {
     origin: true,
     methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
   });
+  app.setGlobalPrefix('api/v1');
+
+  const config = new DocumentBuilder()
+    .setTitle('Compass Events API')
+    .setDescription(
+      'API dedicated to creating, managing, and registering events within the Compass Events platform.',
+    )
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/v1/api-docs', app, documentFactory());
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.API_PORT ?? 3000);

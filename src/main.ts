d15f10229afe432610d1aec410c196e2d15155e4
Alisson Oliveira <alisson.oliver.dev@gmail.com>
2025-06-04 12:38:21 -0300

    feat(main): enable CORS with specified methods for improved API access

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(helmet());
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'PUT', 'PATCH', 'POST', 'DELETE'],
+  });
 
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.API_PORT ?? 3000);
